709e6d97a614e140a9346d1e72a42f754112d515
LuisHeysen <lsalvador.ht@gmail.com>
2025-07-29 12:11:52 -0400

    Revert "first compiler"
    This reverts commit 27903996e195fb60ac2ee21f38232342b632b6e2.
    
    	deleted:    Compiler.py
    	deleted:    __pycache__/AST.cpython-312.pyc
    	deleted:    __pycache__/Compiler.cpython-312.pyc
    	deleted:    __pycache__/Lexer.cpython-312.pyc
    	deleted:    __pycache__/Parser.cpython-312.pyc
    	deleted:    __pycache__/Token.cpython-312.pyc

--- main.py
+++ main.py
@@ -2,18 +2,12 @@ from Lexer import Lexer
 from Parser import Parser
 from AST import Program
 import json
-from Compiler import Compiler
 
-from llvmlite import ir
-import llvmlite.binding as llvm
-from ctypes import CFUNCTYPE, c_int, c_float
-
-LEXER_DEBUG: bool = False
-PARSER_DEBUG: bool = False
-COMPILER_DEBUG: bool = True
+LEXER_DEBUG: bool = True
+PARSER_DEBUG: bool = True
 
 if __name__ == "__main__":
-    with open("tests/compiler.pe") as f:
+    with open("tests/lexer.pe") as f:
         code: str = f.read()
 
     if LEXER_DEBUG:
@@ -26,16 +20,9 @@ if __name__ == "__main__":
     l: Lexer = Lexer(source=code)
     p: Parser = Parser(lexer=l) 
 
-    program: Program = p.parse_program()
-    if len(p.errors) > 0:
-        print("Parser errors:")
-        for error in p.errors:
-            print(f"  {error}")
-        exit(1)
-
-
     if PARSER_DEBUG:
         print("Parser Debug Mode:")
+        program: Program = p.parse_program()
 
         if p.errors:
             print("Parser errors:")
@@ -48,14 +35,3 @@ if __name__ == "__main__":
             json.dump(program.json(), f, indent=4)
 
         print("AST was saved to debug/ast.json")
-
-    c: Compiler = Compiler()
-    c.compile(node=program)
-
-    # Output steps
-    module: ir.Module = c.module
-    module.triple = llvm.get_default_triple()
-
-    if COMPILER_DEBUG:
-        with open("debug/ir.ll", "w") as f:
-            f.write(str(module))
